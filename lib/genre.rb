64b70049702e63f4723515c39bd3636a206bd5ed
Yani Asari <yani.iman1@gmail.com>
2020-09-08 23:58:38 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,38 +1,34 @@
-class Genre 
-    extend Concerns::Findable 
-    
-    attr_accessor :name, :songs 
-    
+class Genre
+    extend Concerns::Findable
+    attr_accessor :name, :songs
+
     @@all = []
-    
+
     def initialize(name)
         @name = name
         @songs = []
-    end 
+    end
 
     def self.all
-      @@all
-    end 
+        @@all
+    end
 
     def self.destroy_all
         self.all.clear
-    end 
-    
-     def save 
-        @@all << self
-    end 
-
-    def self.create(name) 
-      new_genre = Genre.new(name)
-      new_genre.save
-      new_genre
     end
-    
-    #does it need a find_by_name and find_or_create_by_name method?
-    
-    def artists
-      self.songs.map {|song| song.artist}.uniq
-      #self.all.select { |artist| artist.genre == self } 
-    end 
 
-end 
+    def save
+        @@all << self
+      end
+
+    def self.create(name)
+        new_genre = Genre.new(name)
+        new_genre.save
+        new_genre
+    end
+
+    def artists
+        self.songs.map { |song| song.artist }.uniq
+    end
+
+end
