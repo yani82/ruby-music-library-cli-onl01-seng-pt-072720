bfd33b4e23c3a41ffcc7abc9f71bc3eedd15eeeb
Yani Asari <yani.iman1@gmail.com>
2020-09-09 00:04:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,43 +1,43 @@
 class Artist
-    extend Concerns::Findable
-    attr_accessor :name, :songs
+  extend Concerns::Findable
 
-    @@all = []
+  attr_accessor :name
+  attr_reader :songs
 
-    def initialize(name)
-        @name = name
-        @songs = []
-    end
+  @@all = []
 
-    def self.all
-        @@all
-    end
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
 
-    def self.destroy_all
-        self.all.clear
-    end
+  def self.all
+    @@all
+  end
 
-    def save
-        @@all << self
-    end
+  def self.destroy_all
+    all.clear
+  end
 
-    def self.create(name)
-        new_artist = Artist.new(name)
-        new_artist.save
-        new_artist
-    end
+  def save
+    self.class.all << self
+  end
 
-    def add_song(song)
-        if !song.artist
-            song.artist = self
-        end
-        if !@songs.include?(song)
-             @songs << song
-        end
-    end
+  def self.create(name)
+    artist = new(name)
+    artist.save
+    artist
 
-    def genres
-        self.songs.map { |song| song.genre }.uniq
-    end
+    # Or, as a one-liner:
+    # new(name).tap{ |a| a.save }
+  end
 
+  def add_song(song)
+    song.artist = self unless song.artist
+    songs << song unless songs.include?(song)
+  end
+
+  def genres
+    songs.collect{ |s| s.genre }.uniq
+  end
 end
