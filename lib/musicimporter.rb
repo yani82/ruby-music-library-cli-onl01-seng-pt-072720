796488be09c09ea705bd305e46cac13a58eaee67
Yani Asari <yani.iman1@gmail.com>
2020-09-09 00:05:58 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -1,18 +1,15 @@
 class MusicImporter
-    attr_accessor :path
+  attr_reader :path
 
-    def initialize(path)
-        @path = path
-    end
+  def initialize(path)
+    @path = path
+  end
 
-    def files
-        var = Dir.entries(self.path)
-        var.shift(2)
-        var
-    end
+  def files
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+  end
 
-    def import
-        self.files.each { |file| Song.create_from_filename(file) }
-    end
-
+  def import
+    files.each{ |f| Song.create_from_filename(f) }
+  end
 end
